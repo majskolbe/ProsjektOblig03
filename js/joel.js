e3bb28a791e29784b7833428d18c7099c005ddd2
jmarkussen <joel.markussen@hotmail.com>
2023-11-21 14:16:33 +0100

    Hei hallo

--- js/joel.js
+++ js/joel.js
@@ -2,20 +2,19 @@
 
 //drawing rectangles and circles in different sizes and possision. 
 
-circleSvg("100", "200", "100", "fill:white", "joelSvg");
-circleSvg("60", "170", "20", "fill:blue", "joelSvg");
-circleSvg("140", "170", "20", "fill:blue", "joelSvg");
-circleSvg("100", "200", "20", "fill:red", "joelSvg");
-oval("100", "250", "50", "20", "fill:red", "joelSvg");
-
+circleSvg("100", "100", "100", "fill:white", "joelSvg");
+circleSvg("140", "70", "20", "fill:blue", "joelSvg");
+circleSvg("60", "70", "20", "fill:blue", "joelSvg");
+circleSvg("100", "110", "20", "fill:red", "joelSvg");
+oval("100", "160", "50", "20", "fill:red", "joelSvg");
 
 
  //draving circles with different parameters
- circle(100, "solid", "white", 100, 200,"joelCanvas")
- circle(20, "solid", "blue", 140, 170,"joelCanvas")
- circle(20, "solid", "blue", 60, 170,"joelCanvas")
- circle(20, "solid", "red", 100, 200,"joelCanvas")
+ circle(100, "solid", "white", 100, 100, "joelCanvas")
+ circle(20, "solid", "blue", 140, 70, "joelCanvas")
+ circle(20, "solid", "blue", 60, 70, "joelCanvas")
+ circle(20, "solid", "red", 100, 110, "joelCanvas")
 
 
  //drawing mouth with canvas
- smile(100, 250, 50, 20, 400, 0, "solid", "red","joelCanvas");
+ smile(100, 160, 50, 20, 400, 0, "solid", "red", "joelCanvas");
