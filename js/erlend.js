d39a3c3df366279e047cf1f7655140a7bde24c39
majskolbe <majasp@student.uia.no>
2023-11-21 15:07:52 +0100

    ny endring erlend

---
+++ js/erlend.js
@@ -0,0 +1,28 @@
+
+//drawing with SVG
+circleSvg("100", "100", "100", "fill:yellow", "erlendSvg");
+circleSvg("65", "70", "20", "fill:blue", "erlendSvg");
+circleSvg("140", "70", "20", "fill:blue", "erlendSvg")    
+
+rectangleSvg("90", "87", "20", "20", "fill:black", "erlendSvg");
+rectangleSvg("30", "30", "150", "10", "fill:red", "erlendSvg");
+rectangleSvg("65", "140", "83", "10", "fill:blue", "erlendSvg");
+
+    
+    
+   
+//draving with canvas
+circle(100, "solid", "yellow", 100, 100, "erlendCanvas")
+circle(20, "solid", "blue", 140, 70, "erlendCanvas")
+circle(20, "solid", "blue", 60, 70, "erlendCanvas")
+    
+rectangle(10, 120, 40, 140, "solid", "blue", "erlendCanvas")
+rectangle(10, 130, 37, 20, "solid", "red", "erlendCanvas")
+rectangle(18, 20, 90, 85, "solid", "black", "erlendCanvas")
+
+
+
+
+    
+
+
