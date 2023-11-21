72ded8ed060feae330fef45bbf0d84e7d9bd87b8
KristianMB13 <144322698+KristianMB13@users.noreply.github.com>
2023-11-21 14:28:22 +0100

    fix igjen

--- js/figures.js
+++ js/figures.js
@@ -25,7 +25,7 @@ rectangle = function(height, width, x, y, mode, color, id) {
 
 
 Oval = function(x, y, radiusX, radiusY, rotation, startAngle, endAngle, mode, color, id) {
-  const ctx = document.getElementById(id);
+  const ctx = document.getElementById(id).getContext("2d");
   ctx.fillStyle = color;
   ctx.beginPath();
   ctx.ellipse(x, y, radiusX, radiusY, rotation, startAngle, endAngle);
