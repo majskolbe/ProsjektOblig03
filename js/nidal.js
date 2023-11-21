3f7303c833c60ee5f290bbe9b2831ae121f46c51
Dzemalen <nidalalendar7@gmail.com>
2023-11-21 14:21:08 +0100

    kode

---
+++ js/nidal.js
@@ -0,0 +1,23 @@
+circleSvg("100", "100", "100", "fill:yellow", "nidalSvg");
+rectangleSvg("75", "66", "50", "10", "fill:black", "nidalSvg");
+circleSvg("100", "79", "9", "fill:yellow", "nidalSvg");
+rectangleSvg("75", "10", "5", "15", "fill:black", "nidalSvg");
+rectangleSvg("67", "20", "12", "5", "fill:black", "nidalSvg");
+rectangleSvg("67", "20", "5", "15", "fill:black", "nidalSvg");
+rectangleSvg("75", "125", "50", "10", "fill:black", "nidalSvg");
+circleSvg("65", "70", "27", "fill:yellow;stroke:black;stroke-width:1", "nidalSvg");
+circleSvg("135", "70", "27", "fill:yellow;stroke:black;stroke-width:1", "nidalSvg");
+circleSvg("60", "70", "15", "fill:darkblue", "nidalSvg");
+circleSvg("130", "70", "15", "fill:darkblue", "nidalSvg");
+
+
+circle(100, "solid", "yellow", 100, 100, "nidalCanvas")
+circle(25, "fill", "black", 135, 70, "nidalCanvas")
+circle(25, "fill", "black",65, 70, "nidalCanvas")
+circle(15, "solid", "darkblue", 130, 70, "nidalCanvas")
+circle(15, "solid", "darkblue",60, 70, "nidalCanvas")
+
+rectangle(10, 50, 75, 125, "solid", "black", "nidalCanvas")
+rectangle(15, 5, 75, 10, "solid", "black", "nidalCanvas")
+rectangle(5, 15, 65, 20, "solid", "black", "nidalCanvas")
+rectangle(15, 5, 65, 20, "solid", "black", "nidalCanvas")
