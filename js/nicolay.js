ce76d50f8e389edd07e2ad4dad629d9cea6a76d4
NicolayLT <nicolayl@uia.no>
2023-11-21 15:08:46 +0100

    Nicolay JS

---
+++ js/nicolay.js
@@ -0,0 +1,13 @@
+
+rectangle(300, 540, 0, 0, "solid", "lightblue", "NicolayCanvas")    
+rectangle(80, 540, 0, 220, "solid", "darkblue", "NicolayCanvas")
+
+circle(110, "solid", "yellow", 260, 140)
+circle(40, "solid", "orange", 50, 20)
+
+Oval(220, 125, 20, 23, 400, 0, Math.PI * 2, "solid", "orange", "NicolayCanvas")
+Oval(215, 85, 35, 5, -7, 0, Math.PI * 2, "solid", "orange", "NicolayCanvas")
+Oval(310, 125, 22, 5, 180, 0, Math.PI * 2, "solid", "orange", "NicolayCanvas")
+Oval(310, 105, 35, 5, 180, 0, Math.PI * 2, "solid", "orange", "NicolayCanvas")
+Oval(265, 190, 50, 20, 180, 0, Math.PI * 2, "solid", "orange", "NicolayCanvas")
+Oval(265, 180, 46, 21, 180, 0, Math.PI * 2, "solid", "yellow", "NicolayCanvas")
