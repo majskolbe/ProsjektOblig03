03ce554a40ed95c11fc6d1ff4b585090af9c9547
majskolbe <majasp@student.uia.no>
2023-11-22 17:05:27 +0100

    khumari endring

--- js/khumari.js
+++ js/khumari.js
@@ -1,4 +1,4 @@
-/*
+//Drawing in SVG
 circleSvg("100", "100", "100", "fill:purple", "khumariSvg");
 circleSvg("60", "70", "20", "fill:black", "khumariSvg");
 circleSvg("140", "70", "20", "fill:black", "khumariSvg");
@@ -7,8 +7,8 @@ circleSvg("140", "90", "15", "fill:purple", "khumariSvg");
 circleSvg("100", "140", "50", "fill:red", "khumariSvg");
 
 rectangleSvg("50", "85", "100", "60", "fill:purple", "khumariSvg")
-*/
-/*
+
+//drawing in Canvas
 circle(100, "solid", "purple", 100, 100, "khumariCanvas")
 circle(20, "solid", "black", 140, 70, "khumariCanvas")
 circle(20, "solid", "black", 60, 70, "khumariCanvas")
@@ -17,6 +17,6 @@ circle(15, "solid", "purple", 60, 90, "khumariCanvas")
 circle(50, "solid", "red", 100, 140, "khumariCanvas")
 
 rectangle(60, 100, 50, 85, "solid", "purple", "khumariCanvas") 
-*/
+
 
 
